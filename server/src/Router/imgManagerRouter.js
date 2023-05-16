885962366db2ebd797544a917ece51073d3072d1
Huy Pham <pgiahuy653@gmail.com>
2023-05-16 17:48:23 +0700

    fix

--- server/src/Router/imgManagerRouter.js
+++ server/src/Router/imgManagerRouter.js
@@ -7,7 +7,7 @@ imgManagerRouter.get('/get-save-img-of-user',getSaveImg);
 imgManagerRouter.get('/get-img-of-user',getImageOfUser);
 imgManagerRouter.delete('/delete-img',deleteImage);
 // imgManagerRouter.post('/add-new-img',addNewImage);
-imgManagerRouter.post('/add-new-img-file',addNewImageFile);
+// imgManagerRouter.post('/add-new-img-file',addNewImageFile);
 imgManagerRouter.put('/update-info-user',putInfoUser);
 
 module.exports = imgManagerRouter;
