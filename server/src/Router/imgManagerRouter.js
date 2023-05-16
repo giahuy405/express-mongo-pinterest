33e6bdb2bfa841330e564f86086f01a04c4b713f
Huy Pham <pgiahuy653@gmail.com>
2023-05-16 17:56:34 +0700

    fix: tmp

--- server/src/Router/imgManagerRouter.js
+++ server/src/Router/imgManagerRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {getInfoUser ,getSaveImg ,getImageOfUser,deleteImage,putInfoUser} = require("../Controllers/imgManagerController.js");
+const {getInfoUser ,getSaveImg ,getImageOfUser,deleteImage,putInfoUser,addNewImageFile} = require("../Controllers/imgManagerController.js");
 const imgManagerRouter = express.Router();
 
 imgManagerRouter.get('/info-user',getInfoUser);
@@ -7,7 +7,7 @@ imgManagerRouter.get('/get-save-img-of-user',getSaveImg);
 imgManagerRouter.get('/get-img-of-user',getImageOfUser);
 imgManagerRouter.delete('/delete-img',deleteImage);
 // imgManagerRouter.post('/add-new-img',addNewImage);
-// imgManagerRouter.post('/add-new-img-file',addNewImageFile);
+imgManagerRouter.post('/add-new-img-file',addNewImageFile);
 imgManagerRouter.put('/update-info-user',putInfoUser);
 
 module.exports = imgManagerRouter;
